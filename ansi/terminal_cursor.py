7c336b5f5e1dbd420c6e93800d26f5b33e5db87e
thiagola92 <thiagola92@gmail.com>
2020-04-12 17:11:47 -0300

    Terminal cursor

--- ansi/terminal_cursor.py
+++ ansi/terminal_cursor.py
@@ -10,3 +10,52 @@ class TerminalCursor():
       ANSI_CURSOR.get('up', moves) +
       ANSI_CSI.get('cursor up')
     )
+
+  def down(moves=1):
+    sys.stdout.write(
+      ANSI.get('csi') +
+      ANSI_CURSOR.get('down', moves) +
+      ANSI_CSI.get('cursor down')
+    )
+
+  def forward(moves=1):
+    sys.stdout.write(
+      ANSI.get('csi') +
+      ANSI_CURSOR.get('forward', moves) +
+      ANSI_CSI.get('cursor forward')
+    )
+
+  def back(moves=1):
+    sys.stdout.write(
+      ANSI.get('csi') +
+      ANSI_CURSOR.get('back', moves) +
+      ANSI_CSI.get('cursor back')
+    )
+
+  def next_line(moves=1):
+    sys.stdout.write(
+      ANSI.get('csi') +
+      ANSI_CURSOR.get('next line', moves) +
+      ANSI_CSI.get('cursor next line')
+    )
+
+  def previous_line(moves=1):
+    sys.stdout.write(
+      ANSI.get('csi') +
+      ANSI_CURSOR.get('previous line', moves) +
+      ANSI_CSI.get('cursor previous line')
+    )
+
+  def column(column=1):
+    sys.stdout.write(
+      ANSI.get('csi') +
+      ANSI_CURSOR.get('horizontal absolute', column) +
+      ANSI_CSI.get('cursor horizontal absolute')
+    )
+
+  def position(row=1, column=1):
+    sys.stdout.write(
+      ANSI.get('csi') +
+      ANSI_CURSOR.get('position', row, column) +
+      ANSI_CSI.get('cursor position')
+    )
