f320f204646b7a75cc3489c2c251410127402f61
thiagola92 <thiagola92@gmail.com>
2020-04-26 17:00:17 -0300

    Customize the fail return

--- get_from_dict/get_from_dict.py
+++ get_from_dict/get_from_dict.py
@@ -1,9 +1,9 @@
 from deep_get import dget
 
-def get_from_dict(dictionary, keys_and_paths):
+def get_from_dict(dictionary, keys_and_paths, fail=None):
   output = {}
 
   for key, path in keys_and_paths:
-    output[key] = dget(dictionary, path, None)
+    output[key] = dget(dictionary, path, fail)
 
   return output
