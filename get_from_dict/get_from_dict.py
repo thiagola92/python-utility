0fa15768505992f0212f396ac4738ceb240c2b2a
thiagola92 <thiagola92@gmail.com>
2020-04-26 17:06:21 -0300

    Revert last commit

--- get_from_dict/get_from_dict.py
+++ get_from_dict/get_from_dict.py
@@ -1,9 +1,9 @@
 from deep_get import dget
 
-def get_from_dict(dictionary, keys_and_paths, fail=None):
+def get_from_dict(dictionary, keys_and_paths):
   output = {}
 
   for key, path in keys_and_paths:
-    output[key] = dget(dictionary, path, fail)
+    output[key] = dget(dictionary, path, None)
 
   return output
